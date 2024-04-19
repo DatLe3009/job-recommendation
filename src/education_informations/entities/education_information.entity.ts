2e53210d043ef7566213680031bec9602b7b97c9
DatLe3009 <105985045+DatLe3009@users.noreply.github.com>
2024-04-19 15:46:21 +0700

    education_informations: checkDates

--- src/education_informations/entities/education_information.entity.ts
+++ src/education_informations/entities/education_information.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm"
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, BeforeInsert, BeforeUpdate } from "typeorm"
 import { OnlineProfile } from "src/online_profiles/entities"
+import { BadRequestException } from "@nestjs/common"
 
 @Entity('education_informations')
 export class EducationInformation extends BaseEntity {
@@ -20,6 +21,14 @@ export class EducationInformation extends BaseEntity {
 
     @Column({ type: 'date'})
     endDate: Date
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    checkDates() {
+        if (this.startDate && this.endDate && this.startDate > this.endDate) {
+            throw new BadRequestException('startDate must be before endDate');
+        }
+    }
 
     @ManyToOne(() => OnlineProfile, (online_profile) => online_profile.education_informations, {
         onDelete: 'CASCADE', 
