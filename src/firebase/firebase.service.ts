96ffada105102cdcdbdb14e110b39d07a8647dd2
DatLe3009 <105985045+DatLe3009@users.noreply.github.com>
2024-05-04 00:35:34 +0700

    firebaseSerivce: upload, delete, update file by userId

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
 
@@ -11,7 +11,7 @@ export class FirebaseService implements OnModuleInit {
 
     async onModuleInit(): Promise<void> {
         await this.connect();
-        // to do: check connect to the storage bucket
+        // TODO: check connect to the storage bucket
         await this.checkBucketExistence();
     }
 
@@ -39,5 +39,57 @@ export class FirebaseService implements OnModuleInit {
         console.log('Firebase: connect to storage bucket successfully')
     }
 
+    async uploadFileByUserId(file: Express.Multer.File, userId: number, type: string): Promise<string> {
+        const filePath = `user/${userId}/${type}/${file.originalname}`;
+        const fileUpload = this.bucket.file(filePath);
 
+        const fileStream = fileUpload.createWriteStream({
+            metadata: {
+                contenType: file.mimetype,
+            },
+            resumable: true,
+            validation: false,
+            public: true
+        })
+
+        await new Promise((resolve, reject) => {
+            fileStream.on('error', reject);
+            fileStream.on('finish', resolve);
+            fileStream.end(file.buffer);
+        })
+
+        const signedUrl = await fileUpload.getSignedUrl({
+            action: 'read',
+            expires: '01-01-2100'
+        });
+
+        return signedUrl[0];
+    }
+
+    async deleteFileByUserId(userId: number, type: string): Promise<void> {
+        const folderPath = `user/${userId}/${type}`;
+        try {
+            const [files] = await this.bucket.getFiles({ prefix: folderPath });
+            if (files.length === 0) throw new NotFoundException('user data not found')
+            const deletePromises = files.map(file => file.delete());
+            await Promise.all(deletePromises);
+        } catch (error) {
+        throw error;
+        }
+    }
+
+    async updateFileByUserId(file: Express.Multer.File, userId: number, type: string): Promise<string> {
+        // TODO: Delete older files
+        const folderPath = `user/${userId}/${type}`;
+
+        const [files] = await this.bucket.getFiles({ prefix: folderPath });
+        if (files.length > 0) {
+            const deletePromises = files.map(file => file.delete());
+            await Promise.all(deletePromises);
+        }
+
+        // TODO: Upload new file
+        return this.uploadFileByUserId(file, userId, type);
+
+    }
 }
